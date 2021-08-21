628d1d3cd61751dbc232a892609a0bcb51123ccc
Mythic-Monsters <73722034+Mythic-Monsters@users.noreply.github.com>
2021-08-21 14:16:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function Add() {
+    var result = Number(document.getElementById("1").value)+ Number(document.getElementById("2").value);
+    window.alert(result);
+    console.log(result)
+    }
+    function Subtract() {
+    var result = Number(document.getElementById("1").value)- Number(document.getElementById("2").value);
+    window.alert(result);
+    console.log(result)
+    }
+    function Multiply() {
+    var result = Number(document.getElementById("1").value)* Number(document.getElementById("2").value);
+    window.alert(result);
+    console.log(result)
+    }
+    function Divide() {
+    var result = Number(document.getElementById("1").value)/ Number(document.getElementById("2").value);
+    window.alert(result);
+    console.log(result)
+    }
